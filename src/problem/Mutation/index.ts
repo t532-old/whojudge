bf6e6e3ae9310812e1169e5b4f20e0f2d0a3defd
t532 <2037246484@qq.com>
2019-08-17 03:54:46 +0800

    Add API uploadTestcaseFile

--- src/problem/Mutation/index.ts
+++ src/problem/Mutation/index.ts
@@ -1,3 +1,4 @@
 export { createProblem } from './createProblem'
 export { deleteProblem } from './deleteProblem'
 export { updateProblem } from './updateProblem'
+export { uploadTestcaseFile } from './uploadTestcaseFile'
--- src/problem/Mutation/uploadTestcaseFile.ts
+++ src/problem/Mutation/uploadTestcaseFile.ts
@@ -1,1 +1,17 @@
-/* TODO */
+import { ID_Input, Int } from '../../prisma-client'
+import { FileUpload } from 'graphql-upload'
+import { createWriteStream } from 'fs'
+
+interface UploadTestcaseFileInput {
+    id: ID_Input
+    idx: Int
+    file: FileUpload
+    type: 'IN' | 'OUT'
+}
+
+export async function uploadTestcaseFile(_, { id, idx, file, type }: UploadTestcaseFileInput) {
+    const write = createWriteStream(`upload/${type.toLowerCase()}file/${id}-${idx}.${type.toLowerCase()}`)
+    const read = file.createReadStream()
+    read.pipe(write)
+    return new Promise(resolve => read.on('end', () => resolve(true)))
+}
